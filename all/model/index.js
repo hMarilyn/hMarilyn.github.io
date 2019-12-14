0b24e6f35ae3190bb3c886ead86a8ad9104650dd
hyy <hyy@example.com>
2019-12-14 17:03:47 +0800

    第二次提交

---
+++ all/model/index.js
@@ -0,0 +1,25 @@
+
+var current
+$(".gallery img").click(function (e) {
+    e.preventDefault();
+    $("#myModal").modal("show");
+    $("#myModal")
+        .find(".modal-body img")
+        .attr("src", $(this).attr("src"));
+    current = $(this);
+});
+$("#myModal .modal-footer span").eq(1).click(function () {
+    // console.log(current.parent().next().find("img")[0]);
+    if (current.parent().next().find("img")[0]) {
+        current = current.parent().next().find("img")
+    }
+    $("#myModal").find(".modal-body img")
+        .attr("src", current.attr("src"));
+})
+$("#myModal .modal-footer span").eq(0).click(function () {
+    if (current.parent().prev().find("img")[0]) {
+        current = current.parent().prev().find("img")
+    }
+    $("#myModal").find(".modal-body img")
+        .attr("src", current.attr("src"));
+})
